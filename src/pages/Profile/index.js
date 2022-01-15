e15cc5f949f1d1995e2506da442358904e25280b
xiayuushi <xiayuushi@qq.com>
2022-01-15 15:13:00 +0800

    登录模块 - 用户退出

--- src/pages/Profile/index.js
+++ src/pages/Profile/index.js
@@ -1,7 +1,7 @@
 import { Component } from 'react'
 import { Link } from 'react-router-dom'
-import { Grid, Button } from 'antd-mobile'
-import { getToken, isAuth } from '../../utils/auth'
+import { Grid, Button, Modal } from 'antd-mobile'
+import { getToken, isAuth, removeToken } from '../../utils/auth'
 import styles from './index.module.scss'
 
 // 菜单数据
@@ -46,52 +46,70 @@ class Profile extends Component {
       // 登录失败
     }
   }
+
+  logout = () => {
+    Modal.alert('提示', '是否现在退出登录?', [
+      { text: '取消' },
+      { text: '确定', onPress: async () => {
+        await this.$request('/user/logout', 'post', null, {authorization: getToken()})
+        removeToken()
+        this.setState({
+          isLogin: false,
+          userInfo: {
+            avatar: '',
+            nickname: ''
+          }
+        })
+      }}
+    ])
+  }
+
   componentDidMount () {
     this.getUserInfo()
   }
   render () {
     return (
-      <div className={styles['root']}>
+      <div className={ styles['root'] } >
       {/* 个人信息 */}
-        <div className={styles['title']}>
+        <div className={ styles['title'] }>
         <img
-          className={styles['bg']}
-          src={process.env.REACT_APP_URL + '/img/profile/bg.png'}
+          className={ styles['bg'] }
+          src={ process.env.REACT_APP_URL + '/img/profile/bg.png' }
           alt="背景图"
         />
-        <div className={styles['info']}>
-          <div className={styles['myIcon']}>
+        <div className={ styles['info']} >
+          <div className={ styles['myIcon']} >
             <img
-              className={styles['avatar']}
-              src={this.state.avatar || DEFAULT_AVATAR}
+              className={ styles['avatar'] }
+              src={ this.state.avatar || DEFAULT_AVATAR }
               alt="icon"
             />
           </div>
-          <div className={styles['user']}>
-            <div className={styles['name']}>{ this.state.nickname || '游客'}</div>
+          <div className={ styles['user'] }>
+            <div className={ styles['name'] }>{ this.state.nickname || '游客' }</div>
             {
               this.state.isLogin ? 
               (
                 // 用户登录后的渲染结构
                 <>
-                  <div className={styles['auth']}>
-                    <span onClick={this.logout}>退出</span>
+                  <div className={ styles['auth'] }>
+                    <span onClick={ this.logout }>退出</span>
                   </div>
-                  <div className={styles['edit']}>
+                  <div className={ styles['edit'] }>
                     编辑个人资料
-                    <span className={styles['arrow']}>
+                    <span className={ styles['arrow'] }>
                       <i className="iconfont icon-arrow" />
                     </span>
                   </div>
                 </>
               ) : (
                 // 用户未登录时的渲染结构
-                <div className={styles['edit']}>
+                <div className={ styles['edit'] }>
                 <Button
                   type="primary"
                   size="small"
                   inline
-                  onClick={() => this.props.history.push('/login')}
+                  onClick={ () => this.props.history.push('/login') }
                 >
                   去登录
                 </Button>
@@ -105,29 +123,29 @@ class Profile extends Component {
       {/* 九宫格菜单 */}
       {/* 需要跳转路由的使用Link包裹进行跳转，否则只渲染结构 */}
       <Grid
-        data={menus}
-        columnNum={3}
-        hasLine={false}
-        renderItem={item =>
+        data={ menus }
+        columnNum={ 3 }
+        hasLine={ false }
+        renderItem={ item =>
           item.to ? (
-            <Link to={item.to}>
-              <div className={styles['menuItem']}>
-                <i className={`iconfont ${item.iconfont}`} />
-                <span>{item.name}</span>
+            <Link to={ item.to }>
+              <div className={ styles['menuItem'] }>
+                <i className={ `iconfont ${item.iconfont}` } />
+                <span>{ item.name }</span>
               </div>
             </Link>
           ) : (
-            <div className={styles['menuItem']}>
+            <div className={ styles['menuItem'] }>
               <i className={`iconfont ${item.iconfont}`} />
-              <span>{item.name}</span>
+              <span>{ item.name }</span>
             </div>
           )
         }
       />
 
       {/* 加入我们 */}
-      <div className={styles['ad']}>
-        <img src={process.env.REACT_APP_URL + '/img/profile/join.png'} alt="" />
+      <div className={ styles['ad'] }>
+        <img src={ process.env.REACT_APP_URL + '/img/profile/join.png' } alt="" />
       </div>
     </div>
     )
@@ -140,3 +158,5 @@ export default Profile
 // 2、先使用isLogin布尔值判断是否登录，根据登录情况渲染不同的组件
 // -、Q1 未登录 渲染未登录的组件，显示游客与默认的头像，不调用接口请求数据
 // -、Q2 已登录 渲染已登录的组件，显示用户与用户的头像，要调用接口请求数据
+// 3、用户退出：弹窗提示，用户点确定退出时，则调用退出接口从服务端退出，同时清理本地缓存的token
+// -、另外也必须重新更新state中的状态（用于驱动视图更新，渲染退出的视图）
